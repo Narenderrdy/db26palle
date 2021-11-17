f4c5c53f9318abcabfa996637e64934e7b6aec42
Narender Reddy <S544926@NWMISSOURI.EDU>
2021-11-17 01:08:39 -0600

    Added Delete

--- controllers/Hat.js
+++ controllers/Hat.js
@@ -118,3 +118,31 @@ exports.Hat_create_Page = function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.Hat_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Hat.findById(req.query.id)
+        res.render('Hatupdate', { title: 'Hat Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle a delete one view with id from query 
+exports.Hat_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Hat.findById(req.query.id)
+        res.render('Hatdelete', {
+            title: 'Hat Delete', toShow:
+                result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/Hat.js
+++ routes/Hat.js
@@ -14,5 +14,9 @@ router.get('/', function (req, res, next) {
 router.get('/detail', Hat_controlers.Hat_view_one_Page);
 /* GET create Hat page */
 router.get('/create', Hat_controlers.Hat_create_Page);
+/* GET create update page */
+router.get('/update', Hat_controlers.Hat_update_Page);
+/* GET create costume page */
+router.get('/delete', Hat_controlers.Hat_delete_Page);
 
 module.exports = router;
